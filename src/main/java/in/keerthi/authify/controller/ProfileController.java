ce9e690a091f97141066174329e0f421f86b229c
keerthivjr <keerthivasankeerthi43@gmail.com>
2025-11-05 23:08:44 +0530

    Initial commit - Spring Boot backend

---
+++ src/main/java/in/keerthi/authify/controller/ProfileController.java
@@ -0,0 +1,32 @@
+package in.keerthi.authify.controller;
+
+import in.keerthi.authify.io.ProfileRequest;
+import in.keerthi.authify.io.ProfileResponse;
+import in.keerthi.authify.service.EmailService;
+import in.keerthi.authify.service.ProfileService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService profileService;
+    private final EmailService emailService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProfileResponse register(@Valid @RequestBody ProfileRequest request) {
+        ProfileResponse response = profileService.createProfile(request);
+        emailService.sendWelcomeEmail(response.getEmail(), response.getName());
+        return response;
+    }
+
+    @GetMapping("/profile")
+    public ProfileResponse getProfile(@CurrentSecurityContext(expression = "authentication?.name") String email) {
+        return profileService.getProfile(email);
+    }
+}
